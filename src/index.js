4e98cd10c6c52854fddd75c63d2ed18b83db1695
Philip Scott <pscott@zeptohost.com>
2018-01-21 20:55:10 -0800

    Add gravity and ground

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import * as THREE from 'three'
-import { Vec3, World, NaiveBroadphase, Body, Sphere } from 'cannon'
+import { ContactMaterial, Material, Vec3, World, NaiveBroadphase, Plane, Body, Sphere } from 'cannon'
 import { GyroNorm } from '../vendor/gyronorm.complete.min'
 
 const timestep = 1/60
 const world = new World()
-world.gravity.set(0,0,0)
+world.gravity.set(0,0,-9.82)
 world.broadphase = new NaiveBroadphase()
 world.solver.iterations = 10
 
+const groundMaterial = new Material('groundMaterial')
+const ground_ground_cm = new ContactMaterial(groundMaterial, groundMaterial, {
+  friction: 0.8,
+  restitution: 0.3,
+  contactEquationStiffness: 1e8,
+  contactEquationRelaxation: 3,
+  frictionEquationStiffness: 1e8,
+  frictionEquationRegularizationTime: 3,
+});
+
+const groundShape = new Plane()
+const groundBody = new Body({ mass: 0, material: groundMaterial })
+groundBody.addShape(groundShape)
+world.add(groundBody)
+
+
 const shape = new Sphere(2)
 const body = new Body({
-  mass: 1
+  mass: 1,
+  material: groundMaterial
 })
+body.position.set(0,0,100)
 body.addShape(shape)
 
 world.addBody(body)
@@ -33,7 +51,7 @@ const material = new THREE.MeshBasicMaterial({
 const sphere = new THREE.Mesh(geometry, material)
 
 scene.add(sphere)
-camera.position.z = 15
+camera.position.z = 100
 
 function render () {
   requestAnimationFrame(render)
@@ -58,8 +76,8 @@ const gn = new GyroNorm()
 gn.init().then(function () {
   gn.start(function (data) {
     const clamp = Interval(-1, 1)
-    const beta = clamp(data.do.beta / 90)
-    const gamma = clamp(data.do.gamma / 90)
+    const beta = 100 * -clamp(data.do.beta / 90)
+    const gamma = 100 * clamp(data.do.gamma / 90)
 
     body.applyLocalForce(new Vec3(gamma,beta,0), new Vec3(0,0,1)) 
   })
